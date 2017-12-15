ef626694d4a58ed0e0bfe4d3e5fb103a46eb0f09
Alex Shpak <alex-shpak@users.noreply.github.com>
2017-12-15 14:50:20 +0100

    Added dropwizard specific binder

---
+++ src/main/java/net/winterly/dropwizard/hk2bundle/DropwizardBinder.java
@@ -0,0 +1,52 @@
+package net.winterly.dropwizard.hk2bundle;
+
+import com.codahale.metrics.health.HealthCheck;
+import io.dropwizard.Bundle;
+import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.cli.Command;
+import io.dropwizard.lifecycle.Managed;
+import io.dropwizard.lifecycle.ServerLifecycleListener;
+import io.dropwizard.servlets.tasks.Task;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.hk2.utilities.binding.ServiceBindingBuilder;
+
+import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class DropwizardBinder extends AbstractBinder {
+
+    private static final List<Class<?>> dropwizardContracts = Arrays.asList(
+            Bundle.class,
+            ConfiguredBundle.class,
+            Command.class,
+
+            HealthCheck.class,
+            Managed.class,
+            LifeCycle.class,
+            LifeCycle.Listener.class,
+            ServerLifecycleListener.class,
+            Task.class
+    );
+
+    /**
+     * Binds dropwizard specific service class as singleton
+     * <p>
+     * If service type is not one of dropwizard contracts just {@link AbstractBinder#bind(Class)} will be invoked
+     *
+     * @param serviceType dropwizard service class
+     * @param <T>         service type
+     * @return builder for further modification
+     */
+    public <T> ServiceBindingBuilder<T> register(Class<T> serviceType) {
+        ServiceBindingBuilder<T> builder = super.bind(serviceType).to(serviceType);
+
+        dropwizardContracts.stream()
+                .filter(contract -> contract.isAssignableFrom(serviceType))
+                .map(builder::to)
+                .findAny().ifPresent(it -> builder.in(Singleton.class));
+
+        return builder;
+    }
+}
