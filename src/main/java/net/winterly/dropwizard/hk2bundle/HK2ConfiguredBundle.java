83f6f5730f16552cb662d0401bb5fffc525ef6ee
Alex Shpak <alex-shpak@users.noreply.github.com>
2018-02-12 09:33:05 +0100

    Bind application config with all contracts

--- src/main/java/net/winterly/dropwizard/hk2bundle/HK2ConfiguredBundle.java
+++ src/main/java/net/winterly/dropwizard/hk2bundle/HK2ConfiguredBundle.java
@@ -5,10 +5,14 @@ import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.glassfish.hk2.api.ServiceLocator;
+import org.jvnet.hk2.annotations.Contract;
 
 import javax.inject.Inject;
+import java.lang.reflect.Type;
+import java.util.Set;
 
 import static org.glassfish.hk2.utilities.ServiceLocatorUtilities.addOneConstant;
+import static org.glassfish.hk2.utilities.reflection.ReflectionHelper.getAdvertisedTypesFromObject;
 
 /**
  * Configured bundle used to obtain and bind configuration instance into DI container.
@@ -25,7 +29,10 @@ class HK2ConfiguredBundle implements ConfiguredBundle<Configuration> {
 
     @Override
     public void run(Configuration configuration, Environment environment) {
-        addOneConstant(serviceLocator, configuration, null,
-                configuration.getClass(), Configuration.class);
+        Set<Type> contracts = getAdvertisedTypesFromObject(configuration, Contract.class);
+        contracts.add(configuration.getClass());
+        contracts.add(Configuration.class);
+
+        addOneConstant(serviceLocator, configuration, null, contracts.toArray(new Type[0]));
     }
 }
